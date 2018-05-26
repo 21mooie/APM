bfb089e77aaa341210c8adfa6622cddb89ad2703
Muata Nkosi <mnkosi@fandm.edu>
2018-05-26 18:10:25 -0400

    list filtered

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,8 +13,11 @@ export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
-    listFilter: string = 'cart';
+    _listFilter: string = 'cart';
+    
+
     showImage:boolean = false;
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             "productId": 2,
@@ -38,8 +41,29 @@ export class ProductListComponent implements OnInit{
         }
     ]
 
+    constructor(){
+        this.filteredProducts = this.products;
+        this.listFilter='cart';
+        console.log("constructor here");
+    }
+
     ngOnInit(): void{
         console.log("product-list will load");
+    }
+
+    get listFilter(): string{
+        return this._listFilter;
+    }
+
+    set listFilter(value:string){
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product:IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
     toggleImage(): void{
