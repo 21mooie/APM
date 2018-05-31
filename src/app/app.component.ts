32912ec741f010672c7b53a69bab055b5b7d53f4
Muata Nkosi <mnkosi@fandm.edu>
2018-05-30 21:11:43 -0400

    routing basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,13 @@ import { ProductService } from "./products/product.service";
           <a class='navbar-brand'>{{pageTitle}}</a>
           <ul class='nav navbar-nav'>
             <li><a [routerLink] = "['/welcome']">Home</a></li>
-            <li><a [routerLink] = "['/products']"><Product List</a></li>
+            <li><a [routerLink] = "['/products']">Product List</a></li>
           </ul>
         </div>
       </nav>
+      <div class='container'>
+        <router-outlet></router-outlet>
+      </div>  
     </div>
   `,
   providers: [ProductService]
