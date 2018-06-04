91bda79a981ac5ddffe3bf0ec6f962486a4636e4
Muata Nkosi <mnkosi@fandm.edu>
2018-06-03 20:47:55 -0400

    label is home button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,8 @@ import { ProductService } from "./products/services/product.service";
     <div>
       <nav class='navbar navbar-default'>
         <div class='container-fluid'>
-          <a class='navbar-brand'>{{pageTitle}}</a>
+          <a class='navbar-brand' [routerLink] = "['/welcome']">{{pageTitle}}</a>
           <ul class='nav navbar-nav'>
-            <li><a [routerLink] = "['/welcome']">Home</a></li>
             <li><a [routerLink] = "['/products']">Product List</a></li>
           </ul>
         </div>
